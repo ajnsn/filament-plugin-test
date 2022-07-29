c8f8bba0101e64f5260dc20f65a888043282b3bf
Alexander Jensen <a.jensen@24bytes.de>
2022-07-29 11:10:59 +0200

    For testing

--- packages/acme-custom-plugin/src/ServiceProvider.php
+++ packages/acme-custom-plugin/src/ServiceProvider.php
@@ -2,11 +2,16 @@
 
 namespace Acme\CustomPlugin;
 
+use Acme\CustomPlugin\Resources\UserResource;
 use Filament\PluginServiceProvider;
 use Spatie\LaravelPackageTools\Package;
 
 class ServiceProvider extends PluginServiceProvider
 {
+    // protected array $resources = [
+    //     UserResource::class,
+    // ];
+
     public function configurePackage(Package $package): void
     {
         $package->name('acme-custom-plugin');
